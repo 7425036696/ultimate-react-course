6b9f95d1565ac900088980a25a08411ce5e57733
Punyansh Singla <167777471+7425036696@users.noreply.github.com>
2025-02-17 20:23:46 +0530

    Update apiCabins.js
    debugging

--- 17-the-wild-oasis/final-6-final/src/services/apiCabins.js
+++ 17-the-wild-oasis/final-6-final/src/services/apiCabins.js
@@ -13,7 +13,7 @@ export async function getCabins() {
 
 export async function createEditCabin(newCabin, id) {
   const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
-
+console.log(newCabin)
   const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
     "/",
     ""
