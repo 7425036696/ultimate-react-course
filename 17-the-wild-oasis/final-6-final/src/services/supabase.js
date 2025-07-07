bdee0bdc915782fbc0cd5da8b6975ae426258ff2
Punyansh Singla <167777471+7425036696@users.noreply.github.com>
2025-07-07 20:05:44 +0530

    Update supabase.js 2 nd time

--- 17-the-wild-oasis/final-6-final/src/services/supabase.js
+++ 17-the-wild-oasis/final-6-final/src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey =  process.env.SUPABASE_KEY
+export const supabaseUrl = "https://kqhpimjtfpbiqxqhhwik.supabase.co";
+const supabaseKey =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxaHBpbWp0ZnBiaXF4cWhod2lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4OTIzMTQsImV4cCI6MjA2NzQ2ODMxNH0.wOCHpI4dHbUuVqpkdr6i3F1nCwhJSR4acofkJRoLlr4"
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
